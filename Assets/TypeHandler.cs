afaee511ab3212dea22acf13d0779f3b1f626924
HarryLong <harryttt@126.com>
2021-12-25 23:04:39 +0800

    Adding more features on sublevel3

--- Assets/TypeHandler.cs
+++ Assets/TypeHandler.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class TypeHandler : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> subgrounds;
+    [SerializeField] public GameObject currentGound;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(currentGound);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log(collider.name+ "Enter");
-       
+        Debug.Log(collider.name+ " Enter");
+        if(collider.name == "StartPoint")
+            currentGound = collider.transform.parent.gameObject;
+        
     }
 }
